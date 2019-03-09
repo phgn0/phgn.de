096733fa72242b5e7bb0637788ad525e69294d61
Peter Hagen <phgn@protonmail.ch>
2019-03-09 22:35:22 +0100

    Linkify the urls

--- render.mjs
+++ render.mjs
@@ -1,7 +1,8 @@
 import fs from "fs"
+import anchorme from "anchorme"
 
 function read(file) {
-    return fs.readFileSync(file)
+    return fs.readFileSync(file, 'utf8')
 }
 
 function write(file, text) {
@@ -9,22 +10,33 @@ function write(file, text) {
 }
 
 // render text to html
-export default function render(text) {
+export default function render(text, { title }) {
+    text = anchorme.default(text, {
+        attributes: [{
+            name: "target",
+            value: "_blank"
+        }],
+        defaultProtocol: "https://"
+    })
+
     const html = `<!DOCTYPE html>
     <html>
         <head>
             <meta charset="UTF-8">
-            <title>phgn.io</title>
-            <link rel="stylesheet" type="text/css" href="./index.css">
+            <title>${title}</title>
+        <link rel="stylesheet" type="text/css" href="./index.css">
         </head>
         <body>
             <div class="json">${text}</div>
         </body>
-    </html>`
+    </html > `
 
     return html
 }
 
+// import as .txt to control whitespace
 const text = read("info.txt")
-const html = render(text)
+const json = JSON.parse(text)
+
+const html = render(text, json)
 write("./index.html", html)
