6458a2d850dff6131c330b93405fa49c4a3fdd4d
Peter Hagen <phgn@protonmail.ch>
2019-05-05 13:48:20 +0200

    Add minimal google analytics
    Just counts pageviews, location and screensize.
    Google Analytics is literally the only free solution.

--- render.mjs
+++ render.mjs
@@ -1,42 +1,45 @@
-import fs from "fs"
-import anchorme from "anchorme"
+import fs from "fs";
+import anchorme from "anchorme";
 
 function read(file) {
-    return fs.readFileSync(file, 'utf8')
+  return fs.readFileSync(file, "utf8");
 }
 
 function write(file, text) {
-    fs.writeFileSync(file, text)
+  fs.writeFileSync(file, text);
 }
 
 // render text to html
 export default function render(text, { title }) {
-    // turn urls into clickable links
-    text = anchorme.default(text, {
-        attributes: [{
-            name: "target",
-            value: "_blank"
-        }],
-        defaultProtocol: "https://"
-    })
+  // turn urls into clickable links
+  text = anchorme.default(text, {
+    attributes: [
+      {
+        name: "target",
+        value: "_blank"
+      }
+    ],
+    defaultProtocol: "https://"
+  });
 
-    const html = `<!DOCTYPE html>
+  const html = `<!DOCTYPE html>
     <html>
         <head>
             <meta charset="UTF-8">
             <title>${title}</title>
+            <script defer src="minimal_analytics.js"></script>
         <link rel="stylesheet" type="text/css" href="./index.css">
         </head>
         <body>
             <div class="json">${text}</div>
         </body>
-    </html > `
+    </html > `;
 
-    return html
+  return html;
 }
 
-const text = read("info.json")
-const data = JSON.parse(text)
+const text = read("info.json");
+const data = JSON.parse(text);
 
-const html = render(text, data)
-write("./index.html", html)
+const html = render(text, data);
+write("./index.html", html);
